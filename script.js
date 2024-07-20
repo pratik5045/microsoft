9f3f70c87000fb51b5a93b6e3b086322d6756b2a
pratik5045 <162993462+pratik5045@users.noreply.github.com>
2024-07-20 13:01:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var menubutton = document.querySelector('.menu');
+var Nav = document.querySelector('.features');
+
+menubutton.addEventListener("click", function(){
+    Nav.classList.toggle('fr');
+})
